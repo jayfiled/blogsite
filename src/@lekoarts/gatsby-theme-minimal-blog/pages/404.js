fde69279b62fdf1e2428868eb14036d534ea63fd
Joel James <jayfiled@gmail.com>
2020-04-03 06:26:13 +1100

    update syntax highlighing in ecommerce post and add 404 page

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () => (
+  <Layout>
+    <h1>That's the wrong URL = ( </h1>
+    <p>Oops, we couldn't find this page!</p>
+  </Layout>
+)
